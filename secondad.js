359afb08989089c4f8f33c082516015d389e1bd8
Juned KH <9571025+junedkh@users.noreply.github.com>
2021-12-17 18:15:01 +0530

    ads

---
+++ secondad.js
@@ -0,0 +1,14 @@
+(function (d, z, s, c) {
+    s.src = "/ / " + d + " / 400 / " + z;
+    s.onerror = s.onload = E;
+    function E() {
+      c && c();
+      c = null;
+    }
+    try {
+      (document.body || document.documentElement).appendChild(s);
+    } catch (e) {
+      E();
+    }
+  })("offfurreton.com ", 4729045, document.createElement("script"), _hagrtw);
+  
